840d315055f8a5d184a69d4afe57fc8c06214c42
S0DIENYE <peppledavid29@gmail.com>
2023-09-15 06:03:55 +0100

    controllers

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc    Get goals
+// @route   GET  /api/goals
+// @access  private
+const getGoals = (req, res) => {
+    res.status(200).json({ msg: 'Get goals'  })
+}
+
+// @desc    Set goal
+// @route   POST  /api/goals
+// @access  private
+const setGoal = (req, res) => {
+    res.status(200).json({ msg: 'Set goals'  })
+}
+
+// @desc    Update goal
+// @route   PUT  /api/goals:id
+// @access  private
+const updateGoal = (req, res) => {
+    res.status(200).json({msg: `Update goal ${req.params.id}`})
+}
+
+// @desc    Delete goal
+// @route   DELETE  /api/goals:id
+// @access  private
+const deleteGoal = (req, res) => {
+    res.status(200).json({msg: `Delete goal ${req.params.id}`})
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
